1e0a085db34ed3599260d0c181d5ae5e146bd7dc
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-12-06 15:15:56 +0900

    ruby string split

--- code/ruby/string.rb
+++ code/ruby/string.rb
@@ -5,6 +5,69 @@ require("./util.rb")
 assert( "abcd" == "abcd" )
 assert( "abcd" === "abcd" )
 
+#= string-compare-single-and-double-quotes
+#= ja: シングルクォート内部ではエスケープシーケンスがほぼ無効になる
+assert( "a\\na" == 'a\na' )
+
+#= string-compare-percent-q-strings
+#= ja: %q文字列内部ではエスケープシーケンスがほぼ無効になる
+assert( %Q(st\\nring) == %q(st\nring) )
+
+
+#= string-here-document-various
+#= ja: ヒアドキュメントいろいろ
+a = <<End
+hello
+End
+assert( a == "hello\n" )
+b = <<End
+#{a} world
+End
+assert( b == "hello\n world\n")
+c = <<"End" 
+str\ning
+End
+assert( c == "str\ning\n" )
+d = <<'End'
+str\ning
+End
+assert( d == "str\\ning\n" )
+e = <<EEEEEEEEND
+any word
+EEEEEEEEND
+assert( e == "any word\n" )
+f = <<-EOS
+indented
+ EOS
+assert( f == "indented\n")
+
+
+#= string-upcase
+a = "hello"
+b = a.upcase
+assert( b == "HELLO" )
+assert( a == "hello" )
+a.upcase!
+assert( a == "HELLO" )
+
+#= string-downcase
+a = "Hello"
+b = a.downcase
+assert( b == "hello" )
+assert( a == "Hello" )
+a.downcase!
+assert( a == "hello" )
+
+#= string-utf8-split-multibyte
+#= ja: マルチバイト文字を文字単位に分割する
+$KCODE='u'
+s = "よい天気"
+ary = s.split("")
+assert( ary[0] == "よ" )
+assert( ary[1] == "い" )
+assert( ary[2] == "天" )
+assert( ary[3] == "気" )
+
 #= string-not-equal
 assert( "abcd" != "abce" )
 
@@ -47,6 +110,13 @@ assert( ary[0] == "ab" );
 assert( ary[1] == "cd" );
 assert( ary[2] == "ef" );
 
+#= string-split-with-limit
+#= ja: splitの第二引数で最大分割数を指定可能
+ary = "ab,cde,fg".split(",",2)
+assert(ary.size==2)
+assert(ary[0]=="ab")
+assert(ary[1]=="cde,fg")
+
 #= string-cannot-append-fixnum
 #== ja: 整数を文字列にそのまま連結することはできない
 #== en: Fixnum can't be appended after a string
@@ -55,3 +125,8 @@ begin
 rescue
   assert( $!.class == TypeError )
 end
+
+
+# memo:
+# 参考: http://ref.xaio.jp/ruby/classes/string/split
+# ruby-langのはまだ.
