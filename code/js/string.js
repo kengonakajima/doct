988994d5dcfa66721e6762c35a0a4eafb720fd32
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-12-06 16:56:39 +0900

    js string

--- code/js/string.js
+++ code/js/string.js
@@ -16,3 +16,26 @@ assert( ary[0] == "ab" );
 assert( ary[1] == "cd" );
 assert( ary[2] == "ef" );
 
+//= string-replace-with-string
+s = "Hello Hell";
+t = s.replace("H","x");
+assert( s == "Hello Hell" );
+assert( t == "xello Hell" );
+
+//= string-replace-with-regex
+s = "Hello Hell";
+t = s.replace(/Hell/,"x");
+assert( s == "Hello Hell" );
+assert( t == "xo Hell" );
+t = s.replace(/Hell/g,"x");
+assert( s == "Hello Hell" );
+assert( t == "xo x" );
+
+//= string-replace-with-function
+s = "hello world";
+u = s.replace(/o/g, function(str) {
+    return "Y";
+});
+console.log(u);
+assert( u == "hellY wYrld" );
+              
