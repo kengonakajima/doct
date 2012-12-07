b19a1847eee939b6437c3b98ed3dc4ae8fe1630b
Kengo Nakajima <kengo.nakajima@gmail.com>
2012-12-08 07:04:47 +0900

    shorter version string

--- code/c/verprint.c
+++ code/c/verprint.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <string.h>
+
 int main(){
-    printf(__VERSION__);
+    char v[1024];
+    int i;
+    snprintf(v,sizeof(v),"%s",__VERSION__);
+    for(i=0;v[i];i++) if(v[i]==' ' ) v[i]=0;
+    puts(v);
     return 0;
 }
